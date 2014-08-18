175372d214a67eb4b3c96c0b32288297cf61e34a
Charlton Galvarino <charlton@2creek.com>
2014-08-18 14:31:44 -0400

    catalog and spinner

---
+++ catalog.js
@@ -0,0 +1,21 @@
+var catalog = {
+    'years'     : [2011,2012,2013,2014]
+   ,'variables' : ['Temperature','Salinity']
+   ,'sites' : {
+     'CRCOOS' : {
+        'CAP2' : {}
+       ,'FRP2' : {}
+       ,'SUN2' : {}
+     }
+     ,'USF' : {
+        'C10' : {}
+       ,'C12' : {}
+       ,'C13' : {}
+     }
+   }
+};
+
+var defaults = {
+   'year' : 2011
+  ,'var'  : 'Temperature'
+};
