23fb5e85f91d27421b8e65b13c08a88f9dc9eb7a
root <root@ip-173-201-16-202.ip.secureserver.net>
2014-09-08 17:46:47 -0700

    changed files commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-climatology
+flot_graph_examples
 ===========
+
+This is a copy of some initial work by cgalvarino at https://github.com/cgalvarino/climatology using javascript flot graphing library in to display time series graphs initially from ncSOS and thredds ncss web services.  I thought the initial code was a good reference point for demonstrating the flot library and parsing the various xml services to get a quick visual and comparisons to other data sources using the catalog.js file which describes the input sources to the graph selection.
+
+The current demo for this tool is at http://secoora.org/ts_compare/index.html and shows the comparison of an in-situ buoy measurements(using ncSOS access) to daily average model measurements(using ncss access).
+
+The catalog.js entries contain a sourceType attribute which is currently:
+
+- ncSOS
+- ndbcSOS
+- wms , this is the GetFeatureInfo query method for wms
+- ncss , this is netcdf subset query method for gridded netcdf 
+
+Currently to add a new data source to graph, follow one of the above examples for the catalog.js entry and also within the main.js file push a similarly labeled reference(ncSOS,ndbcSOS,wms,ncss) onto the 'reqs' array.
+
+To add additional web service parsing methods, see the sourceType handling under the 'processData' function of main.js
+
+I would like to have everything driven from the catalog.js file or similar json configuration files with listing and toggling of all enabled data sources, but that was more time than I wanted to invest for the moment.
+
+
--- catalog.js
+++ catalog.js
@@ -2,9 +2,24 @@ var catalog = {
    'years'     : [2011,2012,2013,2014]
   ,'variables' : ['Temperature','Salinity']
   ,'sites' : {
-    'CRCOOS' : {
+    'NDBC' : {
+      '41004' : {
+        'sourceType' : 'ndbcSOS'
+        ,'wkt' : 'POINT(-79.09 32.5)'
+        ,'getObs' : function(v,year0,year1) {
+          var vh = {
+             'Temperature' : 'sea_water_temperature'
+            ,'Salinity'    : 'sea_water_salinity'
+          };
+          return {u : 'get.php?http://sdf.ndbc.noaa.gov/sos/server.php?request=GetObservation&service=SOS&version=1.0.0&offering=urn:ioos:station:wmo:41004&observedproperty='+ vh[v] +'&responseformat=text/xml;subtype=%22om/1.0.0%22&eventtime='+year0+'-03-01T00:00Z/'+year1+'-03-29T00:00Z' };
+        }
+      }
+    }
+
+    ,'CRCOOS' : {
       'CAP2' : {
-        'wkt' : 'POINT(-79.62 32.8)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-79.62 32.8)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -14,7 +29,8 @@ var catalog = {
         }
       }
       ,'FRP2' : {
-        'wkt' : 'POINT(-80.4 32.27)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-80.4 32.27)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -23,8 +39,10 @@ var catalog = {
           return {u : 'get.php?http://tds.secoora.org/thredds/sos/carocoops.frp2.buoy_archive.nc?request=GetObservation&service=SOS&version=1.0.0&responseFormat=text/xml;schema%3D"om/1.0.0"&offering=urn:ioos:network:org.secoora:all&procedure=urn:ioos:network:org.secoora:all' + '&eventTime=' + year0 + '-01-01T00:00:00Z/' + year1 + '-12-31T23:59:59Z' + '&observedProperty=' + vh[v]};
         }
       }
+
       ,'SUN2' : {
-        'wkt' : 'POINT(-78.48 33.83)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-78.48 33.83)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -36,7 +54,8 @@ var catalog = {
     }
     ,'USF' : {
        'C10' : {
-        'wkt' : 'POINT(-82.92 27.169)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-82.92 27.169)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -46,7 +65,8 @@ var catalog = {
         }
       }
       ,'C12' : {
-        'wkt' : 'POINT(-83.721 27.498)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-83.721 27.498)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -56,7 +76,8 @@ var catalog = {
         }
       }
       ,'C13' : {
-        'wkt' : 'POINT(-83.073 26.063)'
+        'sourceType' : 'ncSOS'
+        ,'wkt' : 'POINT(-83.073 26.063)'
         ,'getObs' : function(v,year0,year1) {
           var vh = {
              'Temperature' : 'water_temperature'
@@ -69,7 +90,8 @@ var catalog = {
   }
   ,'models' : {
     'SABGOM' : {
-      'getObs' : function(v,year,lon,lat,stat) {
+      'sourceType' : 'ncss'
+      ,'getObs' : function(v,year,lon,lat,stat) {
         var vh = {
            'Temperature' : ['temp','']
           ,'Salinity'    : ['salt','_salt']
@@ -86,11 +108,42 @@ var catalog = {
       }
       ,'wkt' : ''
     }
+
+    ,'HYCOM' : {
+      'sourceType' : 'wms'
+        ,'getObs' : function(v,year,bbox) {
+        var vh = {
+           'Temperature' : ['water_temp']
+          ,'Salinity'    : ['salinity']
+        };
+        return {
+          u : 'get.php?http://ecowatch.ncddc.noaa.gov/thredds/wms/hycom/hycom_reg1_agg/HYCOM_Region_1_Aggregation_best.ncd?&LAYERS='+ vh[v][0] +'&ELEVATION=0&TIME='+year+'-03-04T00:00:00.000Z/'+year+'-09-04T00:00:00.000Z&TRANSPARENT=true&STYLES=boxfill%2Frainbow&CRS=EPSG%3A4326&COLORSCALERANGE=-50%2C50&NUMCOLORBANDS=20&LOGSCALE=false&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&SRS=EPSG%3A4326&BBOX='+ bbox +'&X=1&Y=1&INFO_FORMAT=text/xml&QUERY_LAYERS='+ vh[v][0] +'&WIDTH=2&HEIGHT=2'
+          ,v : vh[v][0]
+        };
+      }
+      ,'wkt' : ''
+    }
+
+    ,'USF_OCG' : {
+      'sourceType' : 'wms'
+        ,'getObs' : function(v,year,bbox) {
+        var vh = {
+           'Temperature' : ['temp']
+          ,'Salinity'    : ['salt']
+        };
+        return {
+          u : 'get.php?http://crow.marine.usf.edu:8080/thredds/wms/WFS_ROMS_NF_model/USF_Ocean_Circulation_Group_West_Florida_Shelf_Daily_ROMS_Nowcast_Forecast_Model_Data_best.ncd?&LAYERS='+ vh[v][0] +'&TIME='+ year +'-01-01T00:00:00.000Z/'+ year +'-12-31T00:00:00.000Z&TRANSPARENT=true&STYLES=boxfill%2Frainbow&CRS=EPSG%3A4326&COLORSCALERANGE=-50%2C50&NUMCOLORBANDS=20&LOGSCALE=false&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&SRS=EPSG%3A4326&BBOX='+ bbox +'&X=1&Y=1&INFO_FORMAT=text/xml&QUERY_LAYERS='+ vh[v][0] +'&WIDTH=2&HEIGHT=2'
+          ,v : vh[v][0]
+        };
+      }
+      ,'wkt' : ''
+    }
+
   }
 };
 
 var defaults = {
-   'year' : 2011
+   'year' : 2013
   ,'var'  : 'Temperature'
-  ,'site' : 'C10'
+  ,'site' : 'SUN2'
 };
